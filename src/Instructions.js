4a672930654ce33225b2e80e5770147df3c46b7f
RamMoni <rammonimailbox@gmail.com>
2025-06-25 17:30:41 +0530

    commiting the project

---
+++ src/Instructions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Instructions extends Component {
+
+  render() {
+    return(
+      <p>Click on an emoji to view the emoji short name.</p>
+    )
+  }
+
+}
+
+export default Instructions;
